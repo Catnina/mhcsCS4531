be36b6f82dfd4b5cadaee873339474bc55075100
blyth017 <blyth017@d.umn.edu>
2015-05-04 17:07:22 -0500

    Added quality metrics to Configuration, added collision avoidance to ModuleBuilder, and added edit method to ModuleBuilder.

--- src/mhcs/model/ModuleMaker.java
+++ src/mhcs/model/ModuleMaker.java
@@ -9,11 +9,19 @@ package mhcs.model;
 
 public class ModuleMaker {
 	private ModuleList list;
+	private Integer X_SIZE = 100;
+	private Integer Y_SIZE = 50;
+	private boolean clear[][] = new boolean[X_SIZE][Y_SIZE];
 	
 	//Constructor
 	public ModuleMaker(ModuleList givenList)
 	{
 		list = givenList;
+		for(Integer i = 0; i < X_SIZE; ++i) {
+			for(Integer j = 0; j < Y_SIZE; ++j) {
+				clear[i][j] = true;
+			}
+		}
 	}
 	
 	//General Methods
@@ -21,17 +29,26 @@ public class ModuleMaker {
 		int id = idNumber.intValue();
 		boolean result = true;
 		Module newModule = null;
-		if(id >= 1 && id <= 40) {
-			newModule = new PlainModule(xCoordinate, yCoordinate, idNumber, turnsToUpright, condition);
-			list.addModule(newModule);
-		} else if( (id >= 61 && id <= 80) || (id >= 91 && id <= 100) || (id >= 111 && id <= 120) || (id >= 131 && id <= 134) || (id >= 141 && id <= 144) || (id >= 151 && id <= 154) || (id >= 161 && id <= 164) || (id >= 171 && id <= 174) || (id >=181 && id <= 184) ) {
-			newModule = new StandardModule(xCoordinate, yCoordinate, idNumber, turnsToUpright, condition);
-			list.addModule(newModule);
-		} else {
-			result = false;
+		if(clear[xCoordinate][yCoordinate]) {
+			if(id >= 1 && id <= 40) {
+				newModule = new PlainModule(xCoordinate, yCoordinate, idNumber, turnsToUpright, condition);
+				list.addModule(newModule);
+			} else if( (id >= 61 && id <= 80) || (id >= 91 && id <= 100) || (id >= 111 && id <= 120) || (id >= 131 && id <= 134) || (id >= 141 && id <= 144) || (id >= 151 && id <= 154) || (id >= 161 && id <= 164) || (id >= 171 && id <= 174) || (id >=181 && id <= 184) ) {
+				newModule = new StandardModule(xCoordinate, yCoordinate, idNumber, turnsToUpright, condition);
+				list.addModule(newModule);
+			} else {
+				result = false;
+			}
 		}
 		
 		return result;
 			
+	}
+	
+	public boolean editModule(Integer oldId, Integer idNumber, Integer xCoordinate, Integer yCoordinate, Integer turnsToUpright, String condition) {
+		Module target = list.getModuleByIdNumber(oldId);
+		clear[target.getXCoordinate()][target.getYCoordinate()] = true;
+		list.removeModule(target);
+		return createModule(idNumber, xCoordinate, yCoordinate, turnsToUpright, condition);
 	}
 }
