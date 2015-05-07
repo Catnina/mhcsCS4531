fb23c7d36173501cbd5fa6162d0ff14f2f340fac
jaymadden <maddenj@d.umn.edu>
2015-05-06 20:02:05 -0500

    quality update 2

--- src/mhcs/view/Gui.java
+++ src/mhcs/view/Gui.java
@@ -71,8 +71,9 @@ public class Gui implements EntryPoint{
 	private Date endDate, currentDate;
 	private ModuleList moduleList; // this is the module list!! It must be passed whenever we add (or remove) modules or print the map
 	private ModuleList configList; // configurations list
+	private ModuleList beforeList; // configurations list
 	private Configuration config;
-	Label quality;
+	private Label quality;
 
 	
 	private Integer caseNumb; //this integer holds which test case we are running from NASA/ESA feed (for User Story 1) 
@@ -216,22 +217,22 @@ public class Gui implements EntryPoint{
           	   public void onClick(ClickEvent event) { 
           		   Button tempButton = (Button) event.getSource();
           		   if(tempButton.getText() == "View Configuration Map") {
-          			   quality.setVisible(true);
+          			   //quality.setVisible(true);
           			   tempButton.setText("View Module Map");
           			   modMap.renderMap(configList);
           		   }
           		   else {
           			   modMap.renderMap(moduleList);
           			   tempButton.setText("View Configuration Map");
-          			   quality.setVisible(false);
+          			   //quality.setVisible(false);
           		   }
           	   }
               });
               
               //CARLOS'S CODE
-              quality = new Label ("Configuration Quality: "+ config.getQuality());
-              quality.setVisible(false);
-              southPanel.add(quality);
+              //quality = new Label ("Configuration Quality: "+ config.getQuality());
+              //quality.setVisible(false);
+              //southPanel.add(quality);
               
 //              Button configMapButton = new Button ("View Configuration Map", new ClickHandler() {
 //           	   public void onClick(ClickEvent event) { 
@@ -242,7 +243,7 @@ public class Gui implements EntryPoint{
 //              mapButtons.add(configMapButton);
               southPanel.add(mapButton);
               
-              /*before.setEnabled(false);
+              before.setEnabled(false);
                southPanel.add(before);
               after = new Button ("After", new ClickHandler() {
                 public void onClick(ClickEvent event) {
@@ -250,7 +251,7 @@ public class Gui implements EntryPoint{
                    }
                 });
                after.setEnabled(false);
-                southPanel.add(after);*/
+                southPanel.add(after);
 
            //*******************************************	   
            //NorthPanel
@@ -678,6 +679,7 @@ public class Gui implements EntryPoint{
 					Configuration config;
 					ConfigurationBuilder cBuild = new ConfigurationBuilder(moduleList);
 					MinConfigurationBuilder mcBuild = new MinConfigurationBuilder(moduleList);
+					beforeList = moduleList;
 
 					//
 					if(choice == 0){
